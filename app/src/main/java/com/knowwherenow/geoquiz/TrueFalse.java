7b3f2f6ff8607ff422af9792328fc94d5c4b315a
djcarlin <dcarlin@tracell.com>
2015-01-06 20:09:33 -0800

    TrueFalse commit.

---
+++ app/src/main/java/com/knowwherenow/geoquiz/TrueFalse.java
@@ -0,0 +1,16 @@
+package com.knowwherenow.geoquiz;
+
+/**
+ * Created by djcarlin on 1/6/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse ( int question, boolean trueQuestion)
+    {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+}
