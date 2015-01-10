54deba47493dc1fc85f1be3eed4bb98b55b92757
djcarlin <dcarlin@tracell.com>
2015-01-09 20:07:58 -0800

    checkAnswer()

--- app/src/main/java/com/knowwherenow/geoquiz/QuizActivity.java
+++ app/src/main/java/com/knowwherenow/geoquiz/QuizActivity.java
@@ -25,18 +25,52 @@ public class QuizActivity extends ActionBarActivity {
     };
 
 
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+
+    }
+
+    private void checkAnswer(boolean userPressedTrue)
+    {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue){
+
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+
+
+
+    }
+
+
+
+    private int mCurrentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
         mTrueButton  = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton  = (Button) findViewById(R.id.next_button);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
 
             }
         });
@@ -44,11 +78,23 @@ public class QuizActivity extends ActionBarActivity {
        mFalseButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+               checkAnswer(false);
 
 
            }
        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            updateQuestion();
+
+        }
+
+        });
+
+        updateQuestion();
     }
 
 
@@ -73,4 +119,9 @@ public class QuizActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
 }
--- app/src/main/java/com/knowwherenow/geoquiz/TrueFalse.java
+++ app/src/main/java/com/knowwherenow/geoquiz/TrueFalse.java
@@ -4,8 +4,8 @@ package com.knowwherenow.geoquiz;
  * Created by djcarlin on 1/6/15.
  */
 public class TrueFalse {
-    private int mQuestion;
 
+    private int mQuestion;
     private boolean mTrueQuestion;
 
     public TrueFalse ( int question, boolean trueQuestion)
